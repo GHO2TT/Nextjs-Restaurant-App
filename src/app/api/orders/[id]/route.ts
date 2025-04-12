1848e0adeefaef52e9de5353ce1cee3370de854b
IYKE <ndavidabjc@gmail.com>
2025-04-12 01:20:25 +0100

    minor fixes

--- src/app/api/orders/[id]/route.ts
+++ src/app/api/orders/[id]/route.ts
@@ -4,18 +4,17 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const PUT = async (
   req: NextRequest,
-  context: { params: { id: string } }
+  { params }: { params: { id: string } }
 ) => {
-  const { id } = context.params;
+  const { id } = params;
 
   try {
     const body = await req.json();
     const updatedOrder = await prisma.order.update({
-      where: {
-        id: id,
-      },
+      where: { id },
       data: { status: body.status },
     });
+
     return NextResponse.json(updatedOrder, { status: 200 });
   } catch (error) {
     console.log(error);
@@ -26,25 +25,19 @@ export const PUT = async (
   }
 };
 
-// Delete
-export const DELETE = async (
-  req: NextRequest,
-  context: { params: { id: string } }
-) => {
-  const { id } = context.params;
+export const DELETE = async ({ params }: { params: { id: string } }) => {
+  const { id } = params;
 
   const session = await getAuthSession();
 
   if (session?.user.isAdmin) {
     try {
       await prisma.order.delete({
-        where: {
-          id: id,
-        },
+        where: { id },
       });
+
       return NextResponse.json("Order Has Been Deleted!", { status: 200 });
     } catch (error) {
-      // console.log(error);
       return NextResponse.json(
         { message: `Something went wrong! || ${error}` },
         { status: 500 }
